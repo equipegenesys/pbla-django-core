0193b130c703a9e22947602cf71a3ff99eb003a5
Carlos Mongenegro <cgmonte@gmail.com>
2021-06-06 19:41:53 +0000

    cookies para previnir coleta de dado repetidas

--- coreapp/static/coreapp/update_data.js
+++ coreapp/static/coreapp/update_data.js
@@ -1,16 +1,30 @@
-console.log(user_list)
-console.log(dash_view_url)
+// console.log(user_list)
+// console.log(dash_view_url)
 
 var arrayLength = user_list.length;
 
 var request = new XMLHttpRequest();
 
-for (var i = 0; i < arrayLength; i++) {
-    console.log(user_list[i]);
-    var url = 'https://analytics.pbl.tec.br/api/integ/gdrive/user/update/records?user_id=' + user_list[i]
-    console.log(url);
-    request.open("POST", url, false);
-    request.send()
-}
+// var cookieEquipe = document.cookie.match(/^(.*;)?\s*6T9J\s*=\s*[^;]+(.*)?$/);
 
-window.location.replace(dash_view_url);
+const regex = new RegExp(`^(.*;)?\s*${tag_equipe}\s*=\s*[^;]+(.*)?$`);
+
+var cookieEquipe = document.cookie.match(regex);
+
+// console.log(cookieEquipe)
+
+if (cookieEquipe === null) {
+
+    for (var i = 0; i < arrayLength; i++) {
+        // console.log(user_list[i]);
+        var url = 'https://analytics.pbl.tec.br/api/integ/gdrive/user/update/records?user_id=' + user_list[i]
+        console.log(url);
+        request.open("POST", url, false);
+        request.send()
+    }
+
+    document.cookie = tag_equipe + "=yes";
+    window.location.replace(dash_view_url);
+} else {
+    window.location.replace(dash_view_url);
+}
